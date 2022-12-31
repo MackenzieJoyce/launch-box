ac292b72082245d47f60130739855e60002f2c11
Mackenzie <mackenziejoyce414@gmail.com>
2022-12-31 18:00:54 -0500

    Edited nav set up. Added styling to links.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Launch Schedule</Link>
-                </li>
-                <li>
-                    <Link to="/iss-tracker">ISS Tracker</Link>
-                </li>
-            </ul>
+        <nav style={styles.nav}>
+            <Link to="/" style={styles.link}>Launch Schedule</Link>
+            <Link to="/iss-tracker" style={styles.link}>ISS Tracker</Link>
         </nav>
     );
+}
+
+const styles = {
+    nav: {
+        padding: "2%",
+    },
+    link: {
+        margin: "0 2%",
+        padding: "3% 2% 2% 2%",
+        backgroundColor: "rgb(244, 244, 244)",
+        border: "1px solid black",
+        borderRadius: "5px",
+    }
 }
