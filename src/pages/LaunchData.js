48fbc4d96dcb5f9a1c95aa9e710e8a1299088c22
Mackenzie <mackenziejoyce414@gmail.com>
2022-12-31 17:02:54 -0500

    Added styling to page layout.

--- src/pages/LaunchData.js
+++ src/pages/LaunchData.js
@@ -35,11 +35,22 @@ export default function LaunchData() {
             {loading ? <p>Loading...</p> : null}
             {error ? <p>There was an error</p> : null}
             {launchData ? launchData.map((launch) => (
-                <div key={launch.result}>
+                <div key={launch.result} style={styles.LaunchData}>
                     <LaunchInfo launch={launch} />
                     <LaunchWeather launch={launch} />
                 </div>
             )) : null}
         </>
     )
+}
+
+const styles = {
+    LaunchData: {
+        width: '80%',
+        margin: '2% auto',
+        backgroundColor: 'rgb(252, 252,252)',
+        border: '1px solid black',
+        borderRadius: '5px',
+        display: 'flex',
+    },
 }
