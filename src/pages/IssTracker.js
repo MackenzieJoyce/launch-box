5dbdbaec6c87d40048584b6a3c54303551cfe767
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-04 12:52:59 -0500

    Commented out the input form for specific city for now.

--- src/pages/IssTracker.js
+++ src/pages/IssTracker.js
@@ -24,36 +24,36 @@ export default function IssTracker() {
         getApiData();
     }, []);
 
-    // Get form input value
-    const cityHandler = (e) => {
-        setCurrentLocation(e.target.value);
-    };
+    // // Get form input value
+    // const cityHandler = (e) => {
+    //     setCurrentLocation(e.target.value);
+    // };
 
-    // Get lat and long from city name
-    const handleCitySubmit = (e) => {
-        e.preventDefault();
-        axios
-            .get(`http://api.openweathermap.org/geo/1.0/direct?q=${currentLocation}&limit=5&appid=607d41f5499e033c1a0dfe791c7b54e4`)
-            .then((response) => {
-                // console.log(response.data);
-                setLat(response.data[0].lat);
-                setLong(response.data[0].lon);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    };
+    // // Get lat and long from city name
+    // const handleCitySubmit = (e) => {
+    //     e.preventDefault();
+    //     axios
+    //         .get(`http://api.openweathermap.org/geo/1.0/direct?q=${currentLocation}&limit=5&appid=607d41f5499e033c1a0dfe791c7b54e4`)
+    //         .then((response) => {
+    //             // console.log(response.data);
+    //             setLat(response.data[0].lat);
+    //             setLong(response.data[0].lon);
+    //         })
+    //         .catch((err) => {
+    //             console.error(err);
+    //         });
+    // };
 
     return (
         <>
             <h1>ISS Tracker</h1>
             <p>Know when to look up!</p>
-            <form onSubmit={handleCitySubmit}>
+            {/* <form onSubmit={handleCitySubmit}>
                 <label>Enter your city</label>
                 <input type="text" onChange={cityHandler} />
                 <span>(Pssst: capitalization matters!)</span>
                 <button type="submit">Submit</button>
-            </form>
+            </form> */}
 
             {issData ? issData.map((iss) => (
                     <div key={iss.id}>
