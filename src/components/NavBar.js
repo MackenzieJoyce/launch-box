a1184695116327bfb8b08a1c71968324f8733594
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-07 18:33:57 -0500

    Changed default value of tab to dynamically change to the current page so when you refresh on a page the active tab will stay colored.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { Tabs } from '@mantine/core';
 
 export default function NavBar() {
+    const [activeTab, setActiveTab] = useState(window.location.pathname);
+
+    const handleChange = (value) => {
+        setActiveTab(value);
+    };
+
     return (
         <nav>
-            <Tabs defaultValue="LaunchData">
+            <Tabs value={activeTab} onTabChange={handleChange} color="red">
                 <Tabs.List position="right">
-                    <Link to="/">
-                        <Tabs.Tab value="LaunchData">
-                            Launch Schedule
-                        </Tabs.Tab>
-                    </Link>
-                    <Link to="/iss-tracker">
-                        <Tabs.Tab value="IssTracker">
-                            ISS Tracker
-                        </Tabs.Tab>
-                    </Link>
+                    <Tabs.Tab value="/" component={Link} to="/">
+                        Launch Schedule
+                    </Tabs.Tab>
+                    <Tabs.Tab value="/iss-tracker" component={Link} to="/iss-tracker">
+                        ISS Tracker
+                    </Tabs.Tab>
                 </Tabs.List>
             </Tabs>
         </nav>
