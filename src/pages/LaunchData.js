3f13b5bd54c8ab05acf5c3a70b51cd4676ce8457
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-07 20:56:42 -0500

    Added code to group launches with the same date_str. In this new array, the date is the key while the launch info is the value. I left a lot of code commented so I can go back in case something doesn't work on a GET that does not have multiple launches on one day.

--- src/pages/LaunchData.js
+++ src/pages/LaunchData.js
@@ -12,18 +12,81 @@ export default function LaunchData() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [activeBullet, setActiveBullet] = useState(0)
+    let launch
+    let sameDate = []
+    let manyLaunch
 
     const getApiData = () => {
         axios
             .get('https://fdo.rocketlaunch.live/json/launches/next/5')
             .then(response => {
                 // console.log("Data: ", response.data.result);
-                setLaunchData(response.data.result)
+                launch = response.data.result
+                // setLaunchData(launch)
                 setLoading(false)
                 // If the first launch is today, set the active bullet to 1
-                if (response.data.result[0].date_str === new Date().toISOString().slice(0, 10).replace(/-/g, "")) {
+                if (launch[0].date_str === new Date().toISOString().slice(0, 10).replace(/-/g, "")) {
                     setActiveBullet(1)
                 }
+
+                while (launch.length > 0) {
+                    let firstLaunch = launch[0]
+                    let firstLaunchDate = firstLaunch.date_str
+
+                    let launchesWithSameDate = launch.filter(launch => launch.date_str === firstLaunchDate)
+
+                    if (launchesWithSameDate.length > 0) {
+                        sameDate.push(launchesWithSameDate)
+                        launch = launch.slice(launchesWithSameDate.length)
+                    }
+                    // else {
+                    //     launch = launch.slice(1)
+                    // }
+                }
+                // console.log("Same Date: ", sameDate);
+
+                // Find index that have an array
+                // let index = sameDate.findIndex((element) => {
+                //     return element.length > 1
+                // })
+
+                // On that index, grab the needed info on every launch
+                // manyLaunch = sameDate[index].map((launch) => {
+                //     return {
+                //         name: launch.name,
+                //         provider: launch.provider.name,
+                //         vehicle: launch.vehicle.name,
+                //         pad: launch.pad.location.name,
+                //         date: launch.date_str,
+                //         win_open: launch.win_open,
+                //     }
+                // })
+                // console.log("One day, many launches: ", manyLaunch)
+
+                // Show the date of a repeated launch
+                // let whatDay = sameDate[index][0].date_str
+                // console.log("What day: ", whatDay);
+
+                // Create new array that will hold objects
+                // Date is key
+                // Launch info is value
+                let newArray = []
+                sameDate.forEach((launch) => {
+                    let date = launch[0].date_str
+                    let launchInfo = launch.map((launch) => {
+                        return {
+                            name: launch.name,
+                            provider: launch.provider.name,
+                            vehicle: launch.vehicle.name,
+                            pad: launch.pad.location.name,
+                            date: launch.date_str,
+                            win_open: launch.win_open,
+                        }
+                    })
+                    newArray.push({ [date]: launchInfo })
+                })
+                console.log("New Array: ", newArray);
+                setLaunchData(newArray)
             })
             .catch(error => {
                 console.log(error)
@@ -36,8 +99,6 @@ export default function LaunchData() {
         getApiData()
     }, [])
 
-    console.log("Launch info: ", launchData)
-
     return (
         <Container>
             <PageHeader title="Launch Schedule" description="View the schedule for the next 5 launches" />
@@ -47,10 +108,10 @@ export default function LaunchData() {
                 {launchData ? launchData.map((launch) => (
                     <Timeline.Item color="red" bulletSize={24} lineVariant="dashed">
                         <InfoBar key={launch.id}>
-                            <div style={styles.launchBox}>
+                            {/* <div style={styles.launchBox}>
                                 <LaunchInfo launch={launch} />
                                 <LaunchWeather launch={launch} />
-                            </div>
+                            </div> */}
                         </InfoBar>
                     </Timeline.Item>
                 )) : null}
