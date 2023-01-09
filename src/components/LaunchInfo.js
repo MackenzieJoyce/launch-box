e8439ae2bce604c74b2f5f67fbb7635151cb7e78
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-09 14:02:57 -0500

    Added MediaQueries to LaunchInfo.

--- src/components/LaunchInfo.js
+++ src/components/LaunchInfo.js
@@ -1,24 +1,32 @@
+import { MediaQuery } from "@mantine/core";
+
 export default function LaunchInfo({ launch }) {
     let launchMonth = launch.date_str.substring(0, 4)
     let launchDay = launch.date_str.substring(4, 7)
     let launchYear = launch.date_str.substring(7, 11)
 
     return (
-        <div style={styles.LaunchSection}>
-            <div style={styles.LaunchDate}>
-                <p style={styles.month}>{launchMonth}</p>
-                <p>{launchDay}</p>
-                <p>{launchYear}</p>
+        <MediaQuery smallerThan="sm" styles={stack}>
+            <div style={styles.LaunchSection}>
+                <MediaQuery smallerThan="sm" styles={stretch}>
+                    <div style={styles.LaunchDate}>
+                        <p style={styles.month}>{launchMonth}</p>
+                        <p>{launchDay}</p>
+                        <p>{launchYear}</p>
+                    </div>
+                </MediaQuery>
+                <MediaQuery smallerThan="md" styles={stretch}>
+                    <div style={styles.LaunchInfo}>
+                        <h3>{launch.provider.name} | {launch.vehicle.name}</h3>
+                        <h4>{launch.name}</h4>
+                        <p>Location: {launch.pad.location.name}</p>
+                        {launch.win_open ?
+                        <p>Launch window: {launch.win_open}</p> :
+                        <p>Launch window not updated</p>}
+                    </div>
+                </MediaQuery>
             </div>
-            <div style={styles.LaunchInfo}>
-                <h3>{launch.provider.name} | {launch.vehicle.name}</h3>
-                <h4>{launch.name}</h4>
-                <p>Location: {launch.pad.location.name}</p>
-                {launch.win_open ?
-                <p>Launch window: {launch.win_open}</p> :
-                <p>Launch window not updated</p>}
-            </div>
-        </div>
+        </MediaQuery>
     )
 }
 
@@ -48,4 +56,15 @@ const styles = {
         textAlign: 'left',
         marginLeft: '3%',
     },
+}
+
+const stack = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+}
+
+const stretch = {
+    width: '100%',
+    margin: '0',
 }
