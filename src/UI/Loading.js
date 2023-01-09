06b8839a95bdf99eff36de9448410045a312bcbf
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-09 12:13:08 -0500

    Added AspectRatio and Image tags from Mantine to Loading component.

--- src/UI/Loading.js
+++ src/UI/Loading.js
@@ -1,10 +1,10 @@
 import loading from "../assets/images/loading.gif"
-import { Container } from "@mantine/core"
+import { AspectRatio, Image } from "@mantine/core"
 
 export default function Loading() {
     return (
-        <Container>
-            <img src={loading} alt="Loading..." />
-        </Container>
+        <AspectRatio ratio={16/9}>
+            <Image src={loading} alt="Loading..." />
+        </AspectRatio>
     )
 }
