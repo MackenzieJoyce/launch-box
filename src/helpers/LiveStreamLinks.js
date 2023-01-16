f8f651f393e8e5809b1f2186902b208e27ae2f37
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-16 09:19:08 -0500

    Added links to livestreams.

---
+++ src/helpers/LiveStreamLinks.js
@@ -0,0 +1,20 @@
+const Links = [
+    {
+        name: 'NASA TV (Official)',
+        url: 'https://www.youtube.com/watch?v=21X5lGlDOfg',
+    },
+    {
+        name: 'NASASpaceflight Channel',
+        url: 'https://www.youtube.com/@NASASpaceflight',
+    },
+    {
+        name: 'SpaceX Launches',
+        url: 'https://www.youtube.com/watch?v=bNAebzSvWt4',
+    },
+    {
+        name: 'Rocket Lab',
+        url: 'https://www.youtube.com/@RocketLabNZ',
+    },
+]
+
+export default Links
