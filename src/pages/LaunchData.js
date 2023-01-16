dd375ff8119cf28a92215df607feb3cd6e56d1ce
Mackenzie <mackenziejoyce414@gmail.com>
2023-01-16 09:32:33 -0500

    Map the links and give MediaQuery.

--- src/pages/LaunchData.js
+++ src/pages/LaunchData.js
@@ -7,6 +7,7 @@ import InfoBar from '../UI/InfoBar';
 import Loading from '../UI/Loading';
 import LaunchInfo from '../components/LaunchInfo';
 import LaunchWeather from '../components/LaunchWeather';
+import LiveStreams from '../helpers/LiveStreamLinks';
 
 export default function LaunchData() {
     const [launchData, setLaunchData] = useState([])
@@ -23,12 +24,12 @@ export default function LaunchData() {
         axios
             .get('https://fdo.rocketlaunch.live/json/launches/next/5')
             .then(response => {
-                // console.log("Data: ", response.data.result);
+                console.log('Data: ', response.data.result);
                 launch = response.data.result
                 setLaunchData(launch)
                 setLoading(false)
                 // If the first launch is today, set the active bullet to 1
-                if (launch[0].date_str === new Date().toISOString().slice(0, 10).replace(/-/g, "")) {
+                if (launch[0].date_str === new Date().toISOString().slice(0, 10).replace(/-/g, '')) {
                     setActiveBullet(1)
                 }
 
@@ -47,7 +48,7 @@ export default function LaunchData() {
                 //     }
                 // }
 
-                // console.log("Same Date: ", sameDate);
+                // console.log('Same Date: ', sameDate);
 
                 // Find index that have an array
                 // let index = sameDate.findIndex((element) => {
@@ -56,7 +57,7 @@ export default function LaunchData() {
 
                 // Show the date of a repeated launch
                 // let whatDay = sameDate[index][0].date_str
-                // console.log("What day: ", whatDay);
+                // console.log('What day: ', whatDay);
 
                 // // make a new array that will hold the needed info for the specific index of sameDate
                 // manyLaunch = sameDate[index].map((launch) => {
@@ -95,7 +96,7 @@ export default function LaunchData() {
                 //     })
                 //     newArray.push({ [date]: launchInfo })
                 // })
-                // console.log("New Array: ", newArray);
+                // console.log('New Array: ', newArray);
 
                 // setLaunchData(sameDate)
             })
@@ -104,43 +105,88 @@ export default function LaunchData() {
                 setError(true)
                 setLoading(false)
             })
-        console.log("Launch Data: ", launchData);
+        console.log('Launch Data: ', launchData);
     }, [])
 
     return (
-        <MediaQuery smallerThan="md" styles={contain}>
-            <Container size="md">
-                <PageHeader title="Launch Schedule" description="View the schedule for the next 5 launches" />
-                {loading ? <Loading /> : null}
-                {error ? <p>There was an error</p> : null}
+        <MediaQuery smallerThan='md' styles={contain}>
+            <Container size='md'>
+                <PageHeader title='Launch Schedule' description='View the schedule for the next 5 launches' />
+                <MediaQuery smallerThan='md' styles={reverseAbove}>
+                <div style={styles.nextTo}>
+                    {loading ? <Loading /> : null}
+                    {error ? <p>There was an error</p> : null}
 
-                <Timeline color="red" radius="lg" active={activeBullet} bulletSize={24} lineWidth={6} style={styles.Timeline}>
-                    {launchData ? launchData.map((launch) => (
-                        <Timeline.Item color="red" bulletSize={24} lineVariant="dashed">
-                            <InfoBar key={launch.id}>
-                                <div style={styles.launchBox}>
-                                    <LaunchInfo launch={launch} />
-                                    <LaunchWeather launch={launch} />
-                                </div>
-                            </InfoBar>
-                        </Timeline.Item>
-                    )) : null}
-                </Timeline>
+                    <Timeline color='red' radius='lg' active={activeBullet} bulletSize={24} lineWidth={6} style={styles.spacing}>
+                        {launchData ? launchData.map((launch) => (
+                            <Timeline.Item color='red' bulletSize={24} lineVariant='dashed'>
+                                <InfoBar key={launch.id}>
+                                    <div style={styles.launchBox}>
+                                        <LaunchInfo launch={launch} />
+                                        <LaunchWeather launch={launch} />
+                                    </div>
+                                </InfoBar>
+                            </Timeline.Item>
+                        )) : null}
+                    </Timeline>
+
+                    <MediaQuery smallerThan='md' styles={aside}>
+                        <aside style={styles.spacing}>
+                            <h3>Livestream Links</h3>
+                            <MediaQuery smallerThan='md' styles={flat}>
+                                <ul>
+                                    { LiveStreams.map((livestream) => {
+                                        return (
+                                                <li key={livestream.name} style={styles.link}>
+                                                    <a href={livestream.url} target='_blank' rel='noreferrer'>{livestream.name}</a>
+                                                </li>
+                                        )
+                                    })
+                                    }
+                                </ul>
+                            </MediaQuery>
+                        </aside>
+                    </MediaQuery>
+                    </div>
+                </MediaQuery>
             </Container>
         </MediaQuery>
     )
 }
 
 const styles = {
-    Timeline: {
-        margin: '0 auto 5% auto',
+    nextTo: {
+        display: 'flex',
+    },
+    spacing: {
+        margin: '0 3%',
     },
     launchBox: {
         display: 'flex',
         padding: '3% 1%',
     },
+    link: {
+        width: 'fit-content',
+        padding: '3% 0',
+    }
 }
 
 const contain = {
-    maxWidth: '90%',
+    maxWidth: '93%',
+    display: 'flex',
+    flexDirection: 'column',
+}
+
+const reverseAbove = {
+    flexDirection: 'column-reverse',
+}
+
+const aside = {
+    marginBottom: '3%',
+}
+
+const flat = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
 }
