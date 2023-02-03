cf7808b34bfc5631502e494c1b4ba6ac90aa5d42
Mackenzie <mackenziejoyce414@gmail.com>
2023-02-03 16:46:02 -0500

    Added other agencies and added a summary for the urls.

--- src/helpers/LiveStreamLinks.js
+++ src/helpers/LiveStreamLinks.js
@@ -8,12 +8,24 @@ const Links = [
         url: 'https://www.youtube.com/@NASASpaceflight',
     },
     {
-        name: 'SpaceX Launches',
+        name: 'SpaceX',
+        summary: 'SpaceX Youtube Channel',
         url: 'https://www.youtube.com/watch?v=bNAebzSvWt4',
     },
     {
         name: 'Rocket Lab',
+        summary: 'Rocket Lab Youtube Channel',
         url: 'https://www.youtube.com/@RocketLabNZ',
+    },
+    {
+        name: 'Roscosmos',
+        summary: 'Roscosmos (Russian Space Agency) Website',
+        url: 'https://www.iafastro.org/membership/all-members/roscosmos.html',
+    },
+    {
+        name: 'ISRO',
+        summary: 'ISRO (Indian Space Research Organisation) Website',
+        url: 'https://www.isro.gov.in/',
     },
 ]
 
